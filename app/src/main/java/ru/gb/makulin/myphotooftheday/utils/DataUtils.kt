f39ae343bacaf708756ec1fe76934a41e544f180
Despair <p.a.makulin@gmail.com>
2021-12-25 15:08:09 +0300

    Произвел рефакторинг кода: перенес создание Callback для марсианских роверов из каждой ViewModel в одно место в DataUtils

--- app/src/main/java/ru/gb/makulin/myphotooftheday/utils/DataUtils.kt
+++ app/src/main/java/ru/gb/makulin/myphotooftheday/utils/DataUtils.kt
@@ -1,11 +1,16 @@
 package ru.gb.makulin.myphotooftheday.utils
 
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import ru.gb.makulin.myphotooftheday.facade.mars.MarsPhotoDTO
 import ru.gb.makulin.myphotooftheday.facade.mars.MarsPhotosListDTO
 import ru.gb.makulin.myphotooftheday.facade.photo.PhotoOfTheDayDTO
 import ru.gb.makulin.myphotooftheday.model.MarsPhoto
 import ru.gb.makulin.myphotooftheday.model.MarsPhotosList
 import ru.gb.makulin.myphotooftheday.model.PhotoOfTheDay
+import ru.gb.makulin.myphotooftheday.viewmodel.AppState
 
 fun convertPhotoOfTheDayDtoToPhotoOfTheDay(photoDTO: PhotoOfTheDayDTO): PhotoOfTheDay {
     return with(photoDTO) {
@@ -25,4 +30,28 @@ fun convertMarsPhotoDtoToMarsPhoto(marsDTO: MarsPhotoDTO): MarsPhoto {
     return with(marsDTO) {
         MarsPhoto(imgSrc)
     }
+}
+
+fun getMartianCallback(liveDataToObserve: MutableLiveData<AppState>): Callback<MarsPhotosListDTO> {
+    val callback = object : retrofit2.Callback<MarsPhotosListDTO> {
+        override fun onResponse(
+            call: Call<MarsPhotosListDTO>,
+            response: Response<MarsPhotosListDTO>
+        ) {
+            if (response.isSuccessful && response.body() != null) {
+                val photoDTO = response.body()
+                photoDTO?.let {
+                    liveDataToObserve.value =
+                        AppState.MarsSuccess(convertMarsPhotosListDtoToMarsPhotosList(it))
+                }
+            } else {
+                liveDataToObserve.value = AppState.Error(response.code().toString())
+            }
+        }
+
+        override fun onFailure(call: Call<MarsPhotosListDTO>, t: Throwable) {
+            liveDataToObserve.value = AppState.Error(t.localizedMessage)
+        }
+    }
+    return callback
 }
--- app/src/main/java/ru/gb/makulin/myphotooftheday/viewmodel/mars/MarsRoverOpportunityViewModel.kt
+++ app/src/main/java/ru/gb/makulin/myphotooftheday/viewmodel/mars/MarsRoverOpportunityViewModel.kt
@@ -2,14 +2,11 @@ package ru.gb.makulin.myphotooftheday.viewmodel.mars
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import retrofit2.Call
-import retrofit2.Response
-import ru.gb.makulin.myphotooftheday.facade.mars.MarsPhotosListDTO
 import ru.gb.makulin.myphotooftheday.facade.mars.MarsRepository
 import ru.gb.makulin.myphotooftheday.facade.mars.MarsRepositoryImpl
 import ru.gb.makulin.myphotooftheday.facade.retrofit.RemoteDataSource
 import ru.gb.makulin.myphotooftheday.utils.MARS_ROVER_OPPORTUNITY_NAME
-import ru.gb.makulin.myphotooftheday.utils.convertMarsPhotosListDtoToMarsPhotosList
+import ru.gb.makulin.myphotooftheday.utils.getMartianCallback
 import ru.gb.makulin.myphotooftheday.viewmodel.AppState
 
 class MarsRoverOpportunityViewModel(
@@ -22,34 +19,10 @@ class MarsRoverOpportunityViewModel(
     fun getMarsPhotos(solNum: Int = 1, page: Int = 1) {
         liveDataToObserve.value = AppState.Loading
         marsRepositoryImpl.getMarsPhotosFromRemote(
-            callback,
+            getMartianCallback(liveDataToObserve),
             MARS_ROVER_OPPORTUNITY_NAME,
             solNum,
             page
         )
     }
-
-    private val callback = object : retrofit2.Callback<MarsPhotosListDTO> {
-        override fun onResponse(
-            call: Call<MarsPhotosListDTO>,
-            response: Response<MarsPhotosListDTO>
-        ) {
-            if (response.isSuccessful && response.body() != null) {
-                val photoDTO = response.body()
-                photoDTO?.let {
-                    liveDataToObserve.value =
-                        AppState.MarsSuccess(convertMarsPhotosListDtoToMarsPhotosList(it))
-                }
-            } else {
-                liveDataToObserve.value = AppState.Error(response.code().toString())
-            }
-        }
-
-        override fun onFailure(call: Call<MarsPhotosListDTO>, t: Throwable) {
-            liveDataToObserve.value = AppState.Error(t.localizedMessage)
-        }
-
-    }
-
-
 }
